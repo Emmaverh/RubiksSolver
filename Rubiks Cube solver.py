2f19492c6f45c91dd90b5880c531a1549b0bced7
Emma Verhagen <emmaverh@gmail.com>
2022-12-20 10:28:04 +0100

    begonnen met hoekpunten gele laag goedzetten

--- Rubiks Cube solver.py
+++ Rubiks Cube solver.py
@@ -3125,8 +3125,34 @@ def Geel_kruis_goedzetten (kubusFormalParameter):
 		kubusFormalParameter=draaiDinv(kubusFormalParameter)
 		print("1. draai het onderste vlak met de klok mee. 2. draai het achterste vlak tegen de klok in. 3. draai het onderste vlak tegen de klok in. 4. draai het achterste vlak met de klok mee. 5. draai het onderste vlak tegen de klok in. 6. draai het achterste vlak tegen de klok in. 7. draai het onderste vlak 2x tegen de klok in. 8. draai het achterste vlak met de klok mee. 9. draai het onderste vlak tegen de klok in")
   
-	return kubusFormalParameter 
+	return GeleHoeken_op_de_goede_plaats_zetten(kubusFormalParameter) 
+
+def GeleHoeken_op_de_goede_plaats_zetten(kubusFormalParameter):
+	print("********** we gaan nu de gele hoeken op de goede plaats zetten: ")
+	if (kubusFormalParameter[2][0]=="OGY" or kubusFormalParameter[2][0]=="YOG" or kubusFormalParameter[2][0]=="GYO") and (kubusFormalParameter[2][2]=="OYB" or kubusFormalParameter[2][2]=="BOY" or kubusFormalParameter[2][2]=="YBO") and (kubusFormalParameter[2][4]=="BYR" or kubusFormalParameter[2][4]=="RBY" or kubusFormalParameter[2][4]=="YRB") and (kubusFormalParameter[2][6]=="RYG" or kubusFormalParameter[2][6]=="YGR" or kubusFormalParameter[2][6]=="GRY"):
+		print("De gele hoeken staan al op de goede plaats") 
+	if (kubusFormalParameter[2][0]=="OGY" or kubusFormalParameter[2][0]=="YOG" or kubusFormalParameter[2][0]=="GYO") and (kubusFormalParameter[2][6]=="RYG" or kubusFormalParameter[2][6]=="YGR" or kubusFormalParameter[2][6]=="GRY") and (kubusFormalParameter[2][2]=="OYB" or kubusFormalParameter[2][2]=="BOY" or kubusFormalParameter[2][2]=="YBO") and (kubusFormalParameter[2][4]=="BYR" or kubusFormalParameter[2][4]=="RBY" or kubusFormalParameter[2][4]=="YRB"):	
+		kubusFormalParameter=draaiDinv(kubusFormalParameter)
+		kubusFormalParameter=draaiF(kubusFormalParameter)
+		kubusFormalParameter=draaiD(kubusFormalParameter)
+		kubusFormalParameter=draaiB(kubusFormalParameter)
+		kubusFormalParameter=draaiDinv(kubusFormalParameter)
+		kubusFormalParameter=draaiFinv(kubusFormalParameter)
+		kubusFormalParameter=draaiD(kubusFormalParameter)
+		kubusFormalParameter=draaiBinv(kubusFormalParameter)
+		kubusFormalParameter=draaiDinv(kubusFormalParameter)
+		kubusFormalParameter=draaiF(kubusFormalParameter)
+		kubusFormalParameter=draaiD(kubusFormalParameter)
+		kubusFormalParameter=draaiB(kubusFormalParameter)
+		kubusFormalParameter=draaiDinv(kubusFormalParameter)
+		kubusFormalParameter=draaiFinv(kubusFormalParameter)
+		kubusFormalParameter=draaiD(kubusFormalParameter)
+		kubusFormalParameter=draaiBinv(kubusFormalParameter)
+		print("1. draai het onderste vlak tegen de klok in. 2. draai het voorste vlak met de klok mee. 3. draai het onderste vlak met de klok mee. 4. draai het achterste vlak met de klok mee. 5. draai het onderste vlak tegen de klok in. 6. draai het voorste vlak tegen de klok in. 7. draai het onderste vlak met de klok mee. 8. draai het achterste vlak tegen de klok in. 9. draai het onderste vlak tegen de klok in. 10. draai het voorste vlak met de klok mee. 11. draai het onderste vlak met de klok mee. 12. draai het achterste vlak met de klok mee. 13. draai het onderste vlak tegen de klok in. 14. draai het voorste vlak tegen de klok in. 15. draai het onderste vlak met de klok mee. 16. draai het achterste vlak tegen de klok in.")
+		#tot hier gewerkt.
+	if kubusFormalParameter[2][1][0]=="O" and kubusFormalParameter[2][3][0]=="G" and kubusFormalParameter[2][5][0]=="B" and kubusFormalParameter[2][7][0]=="R":
 	
+	return kubusFormalParameter
  
 #doorloop kubus en kijk waar "OW" of "WO" zich bevindt
 for kubusLaag in range(0,3):
